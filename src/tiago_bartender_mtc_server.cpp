ed31b6bb69847b869ef7b553724d21fcba9ebd6f
Henning Kayser <henningkayser@mail.de>
2018-09-26 21:20:09 +0200

    Implement place action

--- src/tiago_bartender_mtc_server.cpp
+++ src/tiago_bartender_mtc_server.cpp
@@ -30,6 +30,8 @@
 #include <tiago_bartender_msgs/PickAction.h>
 #include <tiago_bartender_msgs/PourAction.h>
 #include <tiago_bartender_msgs/PlacePickAction.h>
+#include <tiago_bartender_msgs/PlaceAction.h>
+
 
 #include <std_srvs/Empty.h>
 
@@ -53,6 +55,8 @@ public:
 		as_pick_(nh_, "tiago_pick", boost::bind(&TiagoBartender::pick_cb, this, _1), false),
 		as_pour_(nh_, "tiago_pour", boost::bind(&TiagoBartender::pour_cb, this, _1), false),
 		as_place_pick_(nh_, "tiago_place_pick", boost::bind(&TiagoBartender::place_pick_cb, this, _1), false),
+		as_place_(nh_, "tiago_place", boost::bind(&TiagoBartender::place_cb, this, _1), false),
+
 		execute_("execute_task_solution", true)
 	{
 		ROS_INFO("waiting for task execution");
@@ -64,12 +68,17 @@ public:
 
 		// general parameters
 		pourer_length_= pnh.param<double>("pourer_length", 0.015);
+		bottle_height_= pnh.param<double>("bottle_height", 0.25);
+		table_height_= pnh.param<double>("table_height", 0.75); // TODO: load from bringup config
+
 		pre_pour_height_= pnh.param<double>("pre_pour_height", 0.3);
 		pour_angle_= pnh.param<double>("pour_angle_", 2.2);
 		lift_object_min_dist_= pnh.param<double>("lift_object_min_dist", 0.01);
 		lift_object_max_dist_= pnh.param<double>("lift_object_max_dist", 0.1);
 		approach_object_min_dist_= pnh.param<double>("approach_object_min_dist", 0.10);
 		approach_object_max_dist_= pnh.param<double>("approach_object_max_dist", 0.15);
+		place_surface_offset_ = pnh.param<double>("place_surface_offset", 0.01);
+		place_distance_ = pnh.param<double>("place_distance", 0.9);
 
 		// compute gripper grasp frame depending on pourer length
 		double grasp_offset_x= pnh.param<double>("grasp_offset_x", 0.04);
@@ -110,6 +119,7 @@ public:
 		as_pick_.start();
 		as_pour_.start();
 		as_place_pick_.start();
+		as_place_.start();
 
 		ROS_INFO("Bartender MTC actions are available");
 
@@ -797,11 +807,193 @@ public:
 		result.result.result= tiago_bartender_msgs::ManipulationResult::SUCCESS;
 		as_place_pick_.setSucceeded(result);
 	}
+
+	void place_cb(const tiago_bartender_msgs::PlaceGoalConstPtr& goal)
+	{
+		std::map<std::string, moveit_msgs::AttachedCollisionObject> attached_objects = psi_.getAttachedObjects();
+		if(attached_objects.size() != 1){
+			ROS_ERROR_STREAM("Invalid request for PlaceAction - No bottle in gripper to place");
+			tiago_bartender_msgs::PlaceResult result;
+			result.result.result = tiago_bartender_msgs::ManipulationResult::INTERNAL_ERROR;
+			as_place_.setAborted(result, "Invalid request - No bottle in gripper");
+			return;
+		}
+		const std::string place_bottle = attached_objects.begin()->first;
+
+		place_task_.reset( new moveit::task_constructor::Task("place_object") );
+		Task& t= *place_task_;
+		t.loadRobotModel();
+
+		auto sampling_planner = std::make_shared<solvers::PipelinePlanner>();
+		sampling_planner->setProperty("goal_joint_tolerance", 1e-5);
+
+		auto cartesian_planner = std::make_shared<solvers::CartesianPath>();
+		cartesian_planner->setMaxVelocityScaling(1.0);
+		cartesian_planner->setMaxAccelerationScaling(1.0);
+		cartesian_planner->setStepSize(.002);
+
+		t.setProperty("group", "arm_torso");
+		t.setProperty("eef", "gripper");
+		t.setProperty("gripper", "gripper");
+
+		Stage* current_state= nullptr;
+		{
+			auto _current_state = std::make_unique<stages::CurrentState>("current state");
+
+			current_state= _current_state.get();
+			t.add(std::move(_current_state));
+		}
+
+		// PLACE
+
+		{
+			auto stage = std::make_unique<stages::Connect>("move to pre-place pose", stages::Connect::GroupPlannerVector{{"arm_torso", sampling_planner}});
+			stage->setTimeout(15.0);
+			//stage->setPathConstraints(upright_constraint);
+			stage->properties().configureInitFrom(Stage::PARENT);
+
+			t.add( filter_upright( t, std::move(stage) ) );
+		}
+
+		{
+			auto stage = std::make_unique<stages::MoveRelative>("put down object", cartesian_planner);
+			stage->properties().set("marker_ns", "approach_place");
+			stage->properties().set("link", "gripper_grasping_frame");
+			stage->properties().configureInitFrom(Stage::PARENT, {"group"});
+			stage->setMinMaxDistance(.08, .13);
+
+			geometry_msgs::Vector3Stamped vec;
+			vec.header.frame_id = "base_footprint";
+			vec.vector.z = -1.0;
+			stage->setDirection(vec);
+			t.add(std::move(stage));
+		}
+
+		{
+			auto stage = std::make_unique<stages::GeneratePose>("place pose");
+			geometry_msgs::PoseStamped p;
+			p.header.frame_id= "base_footprint";
+			p.pose.orientation.w= 1;
+			p.pose.position.x= 0.9 + grasp_frame_transform_.inverse().translation().x();
+			p.pose.position.y= 0.0;
+			p.pose.position.z= table_height_
+				+ 0.5 * bottle_height_
+				+ grasp_frame_transform_.inverse().translation().z()
+				+ place_surface_offset_;
+			stage->setPose(p);
+			stage->properties().configureInitFrom(Stage::PARENT);
+
+			stage->setMonitoredStage(current_state);
+
+			auto wrapper = std::make_unique<stages::ComputeIK>("place pose kinematics", std::move(stage) );
+			wrapper->setMaxIKSolutions(32);
+			wrapper->setIKFrame(grasp_frame_transform_, "gripper_grasping_frame");
+			wrapper->properties().configureInitFrom(Stage::PARENT, {"eef"});
+			wrapper->properties().configureInitFrom(Stage::INTERFACE, {"target_pose"});
+			t.add(std::move(wrapper));
+		}
+
+		{
+			auto stage = std::make_unique<stages::MoveTo>("release object", sampling_planner);
+			stage->properties().property("group").configureInitFrom(Stage::PARENT, "gripper");
+			stage->setGoal("open");
+			t.add(std::move(stage));
+		}
+
+		{
+			auto stage = std::make_unique<stages::ModifyPlanningScene>("allow gripper->object collision");
+			stage->allowCollisions(place_bottle, t.getRobotModel()->getJointModelGroup("gripper")->getLinkModelNamesWithCollisionGeometry(), false);
+			t.add(std::move(stage));
+		}
+
+		{
+			auto stage = std::make_unique<stages::ModifyPlanningScene>("detach object");
+			stage->detachObject(place_bottle, "gripper_grasping_frame");
+			t.add(std::move(stage));
+		}
+
+		Stage* after_place= nullptr;
+		{
+			auto stage = std::make_unique<stages::MoveRelative>("retreat after place", cartesian_planner);
+			stage->properties().configureInitFrom(Stage::PARENT, {"group"});
+			stage->setMinMaxDistance(.12,.25);
+			stage->setIKFrame("gripper_grasping_frame");
+
+			stage->properties().set("marker_ns", "post-place");
+
+			geometry_msgs::Vector3Stamped vec;
+			vec.header.frame_id= "gripper_grasping_frame";
+			vec.vector.x= -1.0;
+			stage->setDirection(vec);
+
+			after_place= stage.get();
+
+			t.add(std::move(stage));
+		}
+
+		{
+			auto stage = std::make_unique<stages::MoveTo>("move home", sampling_planner);
+			stage->properties().configureInitFrom(Stage::PARENT, {"group"});
+			stage->setGoal(transport_pose_);
+			stage->restrictDirection(stages::MoveTo::FORWARD);
+
+			t.add( filter_upright( t, std::move(stage) ) );
+		}
+
+		t.enableIntrospection();
+
+		try {
+			t.plan(1);
+		}
+		catch(InitStageException& e){
+			ROS_ERROR_STREAM(e);
+			tiago_bartender_msgs::PlaceResult result;
+			result.result.result = tiago_bartender_msgs::ManipulationResult::INTERNAL_ERROR;
+			as_place_.setAborted(result, "Initialization failed");
+			return;
+		}
+
+		if(t.numSolutions() == 0){
+			tiago_bartender_msgs::PlaceResult result;
+
+			result.result.result = tiago_bartender_msgs::ManipulationResult::NO_PLAN_FOUND;
+			as_place_.setAborted(result, "Planning failed");
+
+			return;
+		}
+
+		moveit_task_constructor_msgs::Solution solution;
+		t.solutions().front()->fillMessage(solution);
+
+		//ROS_INFO_STREAM( "last trajectory in solution:\n" << solution.sub_trajectory.back().trajectory );
+
+		if(execute_solutions_){
+			moveit_task_constructor_msgs::ExecuteTaskSolutionGoal execute_goal;
+			execute_goal.task_solution = solution;
+			execute_.sendGoal(execute_goal);
+			execute_.waitForResult();
+			moveit_msgs::MoveItErrorCodes execute_result= execute_.getResult()->error_code;
+
+			if(execute_result.val != moveit_msgs::MoveItErrorCodes::SUCCESS){
+				ROS_ERROR_STREAM("task execution failed and returned: " << execute_.getState().toString());
+				tiago_bartender_msgs::PickResult result;
+				result.result.result = tiago_bartender_msgs::ManipulationResult::EXECUTION_FAILED;
+				as_pick_.setAborted(result, "Execution failed");
+				return;
+			}
+		}
+
+		tiago_bartender_msgs::PlaceResult result;
+		result.result.result= tiago_bartender_msgs::ManipulationResult::SUCCESS;
+		as_place_.setSucceeded(result);
+	}
 private:
 	ros::NodeHandle nh_;
 	actionlib::SimpleActionServer<tiago_bartender_msgs::PickAction> as_pick_;
 	actionlib::SimpleActionServer<tiago_bartender_msgs::PourAction> as_pour_;
 	actionlib::SimpleActionServer<tiago_bartender_msgs::PlacePickAction> as_place_pick_;
+	actionlib::SimpleActionServer<tiago_bartender_msgs::PlaceAction> as_place_;
+
 
 	actionlib::SimpleActionClient<moveit_task_constructor_msgs::ExecuteTaskSolutionAction> execute_;
 
@@ -814,6 +1006,7 @@ private:
 	moveit::task_constructor::TaskPtr pick_task_;
 	moveit::task_constructor::TaskPtr pour_task_;
 	moveit::task_constructor::TaskPtr place_pick_task_;
+	moveit::task_constructor::TaskPtr place_task_;
 
 	moveit_msgs::Constraints upright_constraint_;
 
@@ -824,12 +1017,18 @@ private:
 
 	// ros params
 	double pourer_length_;
+	double bottle_height_;
+	double table_height_;
+
 	double pre_pour_height_;
 	double pour_angle_;
-	double lift_object_min_dist_;
-	double lift_object_max_dist_;
 	double approach_object_min_dist_;
 	double approach_object_max_dist_;
+	double lift_object_min_dist_;
+	double lift_object_max_dist_;
+	double place_surface_offset_;
+	double place_distance_;
+
 
 	Eigen::Affine3d grasp_frame_transform_;
 	Eigen::Vector3d pour_offset_;
